b2755e7e4b3f26e18265bc2734975447ca118ca6
iPwnPancakes <DanielBuenrrostro1997@gmail.com>
2018-01-05 12:12:40 -0800

    Fixed routing issues in src/index.js and added on to TODO.txt

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 let path = require('path');
 let express = require('express');
-let routes = require(path.resolve('Chat', 'src', 'routes', 'api.js'));
+let routes = require(path.resolve('src', 'routes', 'api.js'));
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use('/', express.static(path.resolve('Chat', 'public')));
+app.use('/', express.static(path.resolve('public')));
 app.use('/api', routes);
 
 app.listen(PORT, () => {
