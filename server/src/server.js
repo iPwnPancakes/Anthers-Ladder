c87acd466fc2a466e99f2f40f2abf16b56179a53
iPwnPancakes <DanielBuenrrostro1997@gmail.com>
2018-01-19 19:52:35 -0800

    Got mongodb working

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,13 @@
+// Set up dependencies
 const path = require('path');
 const routes = require('./routes/api.js');
 const express = require('express');
-const mongo = require('mongodb').MongoClient;
 
 // Set up instances
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const MongoClient = require('mongodb').MongoClient;
 
 // Set up variables
 const PORT = process.env.PORT || 8080;
@@ -14,11 +15,22 @@ const DBPORT = process.env.DBPORT || 27017;
 const localhost = process.env.IP || 'localhost';
 const distPath = path.resolve(__dirname, '..', '..', 'browser', 'dist');
 
-// App logic
+// Set up connections
 server.listen(PORT, () => {
     console.log('Listening to port: ' + PORT);
 });
 
+MongoClient.connect('mongodb://localhost:27017/', (err, database) => {
+    if(err) throw Error('Error connecting to database!');
+
+    const db = database.db('anthersladder');
+    db.collection('users').count((err, count) => {
+        if(err) throw err;
+        console.log('Total rows: ' + count);
+    });
+});
+
+// App logic
 io.on('connection', (socket) => {
     socket.on('broadcasted message', (message) => {
         socket.broadcast.emit('broadcasted message', message);
